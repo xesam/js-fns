b009a6155115311ce54f5973cb3ab75f873e8367
xesam <xesamguo@gmail.com>
2020-11-22 00:03:28 +0800

    times.js

--- src/times.js
+++ src/times.js
@@ -3,13 +3,13 @@ function units(milliseconds) {
         throw new Error('milliseconds < 0, milliseconds=' + milliseconds);
     }
     const millisecond = milliseconds % 1000;
-    const seconds = parseInt(milliseconds / 1000);
+    const seconds = Math.floor(milliseconds / 1000);
     const second = seconds % 60;
-    const minutes = parseInt(seconds / 60);
+    const minutes = Math.floor(seconds / 60);
     const minute = minutes % 60;
-    const hours = parseInt(minutes / 60);
+    const hours = Math.floor(minutes / 60);
     const hour = hours % 24;
-    const days = parseInt(hours / 24);
+    const days = Math.floor(hours / 24);
     const day = days % 24;
     return {
         day: day,
